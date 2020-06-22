78f5ff0265f154e9fd46f7d94b5997f9053cba1c
reading-stiener <apradha1@conncoll.edu>
2020-06-21 21:22:23 -0400

    Fixed issues with csv for bounding box annotations

--- generate_annotations.py
+++ generate_annotations.py
@@ -6,6 +6,7 @@ import numpy as np
 import csv
 import json
 from pprint import pprint
+import cv2
 
 # create a list of file paths from the URMP folder
 def audio_filepaths(rootdir):
@@ -21,18 +22,18 @@ def audio_filepaths(rootdir):
 
 def bbox_filepaths(rootdir):
 
-    fields = ['img_path', 'mean_annotation']
+    fields = ['img_path', 'label' , 'topleft_x', 'topleft_y', 'bottomright_x', 'bottomright_y', 'confidence']
     file_img_paths = {}
     file_json_paths = {}
     row = [] 
     ref_folder = ''
-    new_row = {} 
+    image_path = ''
     for r_dir, sub_dir_list, file_list in os.walk(rootdir):
         if r_dir: 
             ref_folder = r_dir
         if file_list: 
             if ".jpg" in file_list[0]:
-                new_row['img_path'] = ref_folder+'/'+file_list[0]
+                image_path = ref_folder+'/'+file_list[0]
             else:
                 inst_count = instrument_count(ref_folder)
                 valid_count = 0  
@@ -53,8 +54,9 @@ def bbox_filepaths(rootdir):
                 if not os.path.exists('mean_annotations'):
                     os.mkdir('mean_annotations')
 
-                new_row['mean_annotation'] = mean_annotation
-                row.append(new_row)
+                #new_row['mean_annotation'] = mean_annotation
+                new_rowlist = create_rows(mean_annotation, image_path)
+                row.extend(new_rowlist)
                 new_row = {}
 
     file_name = 'bbox_annotations_URMP.csv'
@@ -63,7 +65,20 @@ def bbox_filepaths(rootdir):
         writer.writeheader()
         writer.writerows(row)
 
-        
+def create_rows(mean_annotation, img_path):
+    rowlist = []
+    row_dict = {'img_path': img_path} 
+    for annotation in mean_annotation:
+        row_dict['label'] = annotation['label']
+        row_dict['confidence'] = annotation['confidence']
+        row_dict['topleft_x'] = annotation['topleft']['x']
+        row_dict['topleft_y'] = annotation['topleft']['y']
+        row_dict['bottomright_x'] = annotation['bottomright']['x']
+        row_dict['bottomright_y'] = annotation['bottomright']['y']
+        rowlist.append(row_dict)
+        row_dict = {'img_path': img_path}
+    return rowlist
+     
 def add_annotations(total, new_annotation, inst_count):
     for i in range(inst_count): 
         total[i]['bottomright']['x'] += new_annotation[i]['bottomright']['x']
@@ -94,7 +109,23 @@ def instrument_count(filepath):
         count =  count + filepath.count(inst)
     return count
 
+def draw_bbox(file_name): 
+    with open(file_name, 'r') as csvfile:
+        reader =  csv.DictReader(csvfile)
 
+        for row in reader:
+            #print(row)
+            file_name = row['img_path']
+            img = cv2.imread(file_name)
+
+            top_left = (int(row['topleft_x']), int(row['topleft_y']))
+            bottom_right = (int(row['bottomright_x']), int(row['bottomright_y']))
+            print(top_left)
+            img = cv2.rectangle(img,top_left,bottom_right,(0,255,0),3)
+            cv2.imshow('image', img)
+            cv2.waitKey(0)
+
+            
 # produce all annotations in a csv file
 def audio_silence_annotations(file_paths):
     fields =  ['file_name', 'silence_list', 'silence_percent']
@@ -128,6 +159,6 @@ def darkflow_annotations(file_paths):
 
 path = 'URMP_JSON'
 #print(instrument_count('URMP_JSON/Fugue_Vn. Vn. Va. Vc/JSONS/002263.json'))
-bbox_filepaths(path)
+#bbox_filepaths(path)
 #audio_silence_annotations(file_paths)
-
+draw_bbox('bbox_annotations_URMP.csv')
